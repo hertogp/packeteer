6395d8a02c2ec60757db4e3894aa97dd4a476550
hertogp <git.hertogp@gmail.com>
2024-06-21 05:38:20 +0200

    adds sint test

--- test/packeteer_test.exs
+++ test/packeteer_test.exs
@@ -2,46 +2,51 @@ defmodule PacketeerTest do
   use ExUnit.Case
   doctest Packeteer
 
+  defmodule Primitives do
+    import Packeteer
+    fixed("uint_", fields: [a: uint(8)], defaults: [a: 128])
+    fixed("sint_", fields: [a: sint(8)], defaults: [a: 127])
+  end
+
   describe "fixed primitives" do
     test "uint" do
-      code = """
-      defmodule Uint do
-        import Packeteer
-
-        fixed("",
-        fields: [
-          a: uint(8)
-        ],
-        defaults: [
-          a: 128
-        ]
-        )
-        end
-      """
-
-      [{uint, _}] = Code.compile_string(code)
+      alias Primitives, as: P
 
       # encode using default values
-      assert <<128>> == uint.encode([])
+      assert <<128>> == P.uint_encode([])
 
       # encode using keyword list
       for n <- 0..255,
-          do: assert(<<n>> == uint.encode(a: n))
+          do: assert(<<n>> == P.uint_encode(a: n))
 
-      # encode ignores unspecified fields
-      assert <<127>> == uint.encode(a: 127, b: 42)
+      # ignores unspecified fields
+      assert <<127>> == P.uint_encode(a: 127, b: 42)
 
       # encode unsigned
-      assert <<255>> == uint.encode(a: -1)
-      assert <<128>> == uint.encode(a: -128)
-      assert <<0>> == uint.encode(a: -256)
+      assert <<255>> == P.uint_encode(a: -1)
+      assert <<128>> == P.uint_encode(a: -128)
+      assert <<0>> == P.uint_encode(a: -256)
 
       # decode returns {offset, kw-list, binary}
       for n <- 0..255,
-          do: assert({8, [a: n], <<n>>} == uint.decode(0, <<n>>))
+          do: assert({8, [a: n], <<n>>} == P.uint_decode(0, <<n>>))
 
-      # decode ignores remaining bits
-      assert {8, [a: 0], <<0, 42>>} == uint.decode(0, <<0, 42>>)
+      # matches part of binary
+      assert {8, [a: 0], <<0, 42>>} == P.uint_decode(0, <<0, 42>>)
+    end
+
+    test "sint" do
+      alias Primitives, as: P
+
+      assert <<127>> = P.sint_encode([])
+      assert <<255>> = P.sint_encode(a: -1)
+      # matches part of binary
+      assert {8, [a: 0], <<0, 42>>} == P.uint_decode(0, <<0, 42>>)
+
+      for n <- -128..127 do
+        assert(<<n>> == P.sint_encode(a: n))
+        assert({8, [a: n], <<n>>} == P.sint_decode(0, <<n>>))
+      end
     end
   end
 end
